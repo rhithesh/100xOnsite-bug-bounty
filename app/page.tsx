3fa3550d5bae7251720e8bb837e8ec47ea5704ff
Kartik Saini <kartik8942@gmail.com>
2024-08-22 20:19:51 +0530

    fix: use className instead of class

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,14 @@
 //@ts-nocheck
-"use client";
+'use client';
 
-import TodoForm from "../component/TodoForm";
-import TodoList from "../component/TodoList";
-import { useState } from "react";
+import TodoForm from '../component/TodoForm';
+import TodoList from '../component/TodoList';
+import { useState } from 'react';
 
 export default function Home() {
   const [todos, setTodos] = useState();
 
-  const addTodo = (text) => {
+  const addTodo = text => {
     const newTodo = {
       id: Math.random(),
       text,
@@ -17,21 +17,21 @@ export default function Home() {
     setTodos([...todos, newTodo]);
   };
 
-  const toggleComplete = (id) => {
+  const toggleComplete = id => {
     setTodos(
-      todos.map((todo) =>
+      todos.map(todo =>
         todo.id === id ? { ...todo, completed: !todo.completed } : todo
       )
     );
   };
 
-  const deleteTodo = (id) => {
-    setTodos(todos.filter((todo) => todo.id != id));
+  const deleteTodo = id => {
+    setTodos(todos.filter(todo => todo.id != id));
   };
 
   return (
-    <div class="container mx-auto px-4 py-8">
-      <h1 class="text-3xl font-bold mb-6">Todo List</h1>
+    <div className='container mx-auto px-4 py-8'>
+      <h1 className='text-3xl font-bold mb-6'>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
       <TodoList
         todos={todos}
