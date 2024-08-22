d9bf3bb4acf9b7036605b446eca84c5fed49598a
Somashekar B R <sanjayrao161@gmail.com>
2024-08-22 19:58:01 +0530

    fixed bug where todos state initial value was undefined casing todos.map error

--- component/TodoList.js
+++ component/TodoList.js
@@ -4,14 +4,15 @@ import TodoItem from "./TodoItem";
 const TodoList = ({ todos, onToggleComplete, onDelete }) => {
   return (
     <ul className="list-none p-0">
-      {todos.map((todo) => (
-        <TodoItem
-          key={todo.id}
-          todo={todo}
-          onToggleComplete={onToggleComplete}
-          onDelete={onDelete}
-        />
-      ))}
+      {todos.length > 0 &&
+        todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggleComplete={onToggleComplete}
+            onDelete={onDelete}
+          />
+        ))}
     </ul>
   );
 };
