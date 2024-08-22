e830ce8fad682610aa458540c557a6761294b76b
Ankur02Sarkar <sarkarrohonankur@gmail.com>
2024-08-22 20:02:15 +0530

    added ts support for todolist

--- component/TodoList.tsx
+++ component/TodoList.tsx
@@ -1,9 +1,25 @@
 import React from "react";
 import TodoItem from "./TodoItem";
 
-const TodoList = ({ todos, onToggleComplete, onDelete }) => {
-  console.log("todos : ", todos);
-  
+interface Todo {
+  id: number;
+  text: string;
+  completed: boolean;
+}
+
+interface TodoListProps {
+  todos: Todo[];
+  onToggleComplete: (id: number) => void;
+  onDelete: (id: number) => void;
+}
+
+const TodoList: React.FC<TodoListProps> = ({
+  todos,
+  onToggleComplete,
+  onDelete,
+}) => {
+  console.log("todos:", todos);
+
   return (
     <ul className="list-none p-0">
       {todos?.map((todo) => (
